c93bb6ea94a53ee11a538960c496c07356276ee1
depthzer0 <carngarn@gmail.com>
2017-06-28 17:06:05 +0300

    t2

--- test_1/test_1.go
+++ test_1/test_1.go
@@ -2,8 +2,16 @@ package main
 
 import(
 	"fmt"
+	"unsafe"
 )
 
 func main(){
-	fmt.Println(1)
+
+	var a int8
+	var b int
+	
+	fmt.Printf("a Sizeof "); fmt.Println(unsafe.Sizeof(a))
+	fmt.Printf("b Sizeof "); fmt.Println(unsafe.Sizeof(b))
+	fmt.Printf("int32(1) Sizeof "); fmt.Println(unsafe.Sizeof(int32(1)))
+	fmt.Printf("1 000 000 000 Sizeof "); fmt.Println(unsafe.Sizeof(1000000000))
 }
